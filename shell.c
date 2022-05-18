f056301754cb4ca6f734dca200a07f7da021e1ec
Rodrigue Mbiabo <rodriguembiabo@gmail.com>
2022-05-19 00:18:41 +0100

    simple shell

--- shell.c
+++ shell.c
@@ -1,29 +1,45 @@
-#include "main.h"
-
 /**
- * main - start shell.
- *
- * Return: (0).
+ * Auth: Adewole Shobanke
+ *     : Herbert Olinga
  */
+#include "shell.h"
 
-int main(void)
+void shell(int ac, char **av, char **env)
 {
-	char *row = NULL;
-	char **tokens = NULL;
-	int ct_output = 0;
+	char *line;
+	char **args;
+	int status = 1;
+	char *tmp = NULL;
+	char *er;
+	char *filename;
+	int flow;
 
-	while (1)
-	{
-		if (isatty(STDIN))
-			write(STDOUT, "#holbies$ ", 10);
-
-		row = rd_row();
-		tokens = tokenizer(row);
-		ct_output = Run(tokens, ct_output, row);
-
-		free(row);
-		free(tokens);
-	}
-
-	return (0);
+	er = "Error";
+	do {
+		prompt();
+		line = _getline();
+		args = split_line(line);
+		flow = bridge(args[0], args);
+		if (flow == 2)
+		{
+			filename = args[0];
+			args[0] = find_path(args[0], tmp, er);
+			if (args[0] == er)
+			{
+				args[0] = search_cwd(filename, er);
+				if (args[0] == filename)
+					write(1, er, 5);
+			}
+		}
+		if (args[0] != er)
+			status = execute_prog(args, line, env, flow);
+		free(line);
+		free(args);
+	} while (status);
+	if (!ac)
+		(void)ac;
+	if (!av)
+		(void)av;
+	if (!env)
+		(void)env;
 }
